0b38ccfc54e67ff4981091603b7481d36331bbed
David Marín <ddmarin94@gmail.com>
2019-06-27 19:43:02 +0200

    fixed webpack issue

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,5 +1,46 @@
 import React from 'react';
 
-const App = () => <div>app</div>;
+const App = () => (
+  <main>
+    <h1>Seguro lorem ipsum</h1>
+    <p>
+      Nulla fermentum libero sit amet urna facilisis pretium. Proin imperdiet imperdiet vestibulum.
+      Praesent mollis justo vel suscipit dapibus. Curabitur dolor neque, luctus in molestie a,
+      condimentum sit amet dolor.
+    </p>
+    <section>
+      <div>
+        <h3>
+          Básico <span>XX,XX€</span>
+        </h3>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.</p>
+        <div>
+  <input type="checkbox" />
+          <p>Praesent tincidunt aliquet urna por XX,XX€</p>
+        </div>
+      </div>
+      <div>
+        <h3>
+          Premium <span>XX,XX€</span>
+        </h3>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.</p>
+        <div>
+          <input type="checkbox" />
+          <p>Praesent tincidunt aliquet urna por XX,XX€</p>
+        </div>
+      </div>
+      <div>
+        <h3>
+          Top <span>XX,XX€</span>
+        </h3>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod.</p>
+        <div>
+          <input type="checkbox" />
+          <p>Praesent tincidunt aliquet urna por XX,XX€</p>
+        </div>
+      </div>
+    </section>
+  </main>
+);
 
 export default App;

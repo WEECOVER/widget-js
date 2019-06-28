14c0453ea48c40ee8e1a3889013d42e66f7d7da0
David Marín <ddmarin94@gmail.com>
2019-06-28 23:00:50 +0200

    Added event bus to stablish comunication between app and widget

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import { loadCss } from './utils';
 import './main.css';
+import EventBus from './utils/event-bus';
 
+window.WidgetEventBus = EventBus;
 loadCss();
+
+EventBus.subscribe('loadData', data => {
+  console.log('data has been loaded', data);
+});
 
 ReactDOM.render(<App />, document.getElementById('widget-root'));

5a699cbcc616671d2cfda6d860db6179ba0b4444
David Marín <ddmarin94@gmail.com>
2019-07-13 15:40:49 +0200

    test

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,9 +4,106 @@ import Header from './Header';
 import Footer from './Footer';
 import { handleInsuranceSelected, removeComplements } from './handlers/handle-insurances';
 import { getPrice } from './handlers/handle-price';
-import { fakeData, singleFakeData } from './fakeData';
 import { applyInitialConfig } from './handlers/handle-initial-config';
 import { getInsuranceList } from '../../services/core';
+
+const fakeData = [
+  {
+    mainTitle: 'Seguro Lorem Ipsum',
+    mainDescription: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.',
+    insurances: [
+      {
+        id: '1',
+        title: 'Básico',
+        price: '49.99',
+        currency: '€',
+        checked: false,
+        description: 'Integer enim tellus, dapibus quis dui quis, adam accumsan posuere ipsum.',
+        complements: [
+          {
+            insuranceId: '1',
+            id: '1.1',
+            checked: false,
+            price: '29.99',
+            currency: '€',
+            description: 'Praesent tincidunt aliquet urna por'
+          }
+        ]
+      },
+      {
+        id: '2',
+        title: 'Premium',
+        price: '49.99',
+        currency: '€',
+        checked: false,
+        description: 'Integer enim tellus, dapibus quis dui quis, adam accumsan posuere ipsum.',
+        complements: [
+          {
+            insuranceId: '2',
+            id: '2.2',
+            checked: false,
+            price: '29.99',
+            currency: '€',
+            description: 'Praesent tincidunt aliquet urna por'
+          }
+        ]
+      },
+      {
+        id: '3',
+        title: 'Top',
+        price: '49.99',
+        currency: '€',
+        checked: false,
+        description: 'Integer enim tellus, dapibus quis dui quis, adam accumsan posuere ipsum.',
+        complements: [
+          {
+            insuranceId: '3',
+            id: '3.3',
+            checked: false,
+            price: '29.99',
+            currency: '€',
+            description: 'Praesent tincidunt aliquet urna por'
+          }
+        ]
+      }
+    ]
+  }
+];
+
+const singleFakeData = [
+  {
+    mainTitle: 'Seguro Lorem Ipsum',
+    mainDescription: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.',
+    insurances: [
+      {
+        id: '1',
+        title: 'Básico',
+        price: '49.99',
+        checked: false,
+        currency: '€',
+        description: 'Integer enim tellus, dapibus quis dui quis, adam accumsan posuere ipsum.',
+        complements: [
+          {
+            insuranceId: '1',
+            id: '1.1',
+            checked: false,
+            price: '29.99',
+            currency: '€',
+            description: 'Praesent tincidunt aliquet urna por'
+          },
+          {
+            insuranceId: '1',
+            id: '1.2',
+            checked: false,
+            price: '29.99',
+            currency: '€',
+            description: 'Praesent tincidunt aliquet urna por'
+          }
+        ]
+      }
+    ]
+  }
+];
 
 const availableStyles = {
   uncompressed: 'uncompressed',

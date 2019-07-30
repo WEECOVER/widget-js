813da1a57105bd21f8708207252d8cf82f732457
cristiam <cristiam86@gmail.com>
2019-07-30 15:49:57 +0200

    update api URL and set https

--- src/services/core/constants.js
+++ src/services/core/constants.js
@@ -1,5 +1,5 @@
-const BASE_URL = 'http://ec2-63-32-56-252.eu-west-1.compute.amazonaws.com';
-const API_VERSION = '/api/1/';
+const BASE_URL = 'https://api.weecover.com';
+const API_VERSION = '/api/1';
 const AVAILABLES_LANG = ['ES', 'CA', 'EN'];
 
 export { BASE_URL, API_VERSION, AVAILABLES_LANG };

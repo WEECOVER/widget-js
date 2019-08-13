07b33e1e56e8425f672bf11c6a8ea46ad9293f57
David Marín <ddmarin94@gmail.com>
2019-08-13 16:15:36 +0200

    css modules added in prod mode

--- .webpack/webpack.production.js
+++ .webpack/webpack.production.js
@@ -14,7 +14,16 @@ module.exports = () => ({
     rules: [
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+        use: [MiniCssExtractPlugin.loader,
+          {
+            loader:'css-loader',
+            options: {
+              modules: {
+                localIdentName: '[local]___[hash:base64:5]'
+              }
+            }
+          },
+        ],
       },
     ]
   },
